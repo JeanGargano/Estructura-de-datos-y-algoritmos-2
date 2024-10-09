bb10de57899f3194265b489f49fe3e6bdbf75aea
JeanGargano <159099449+JeanGargano@users.noreply.github.com>
2024-10-09 17:27:36 -0500

    Mi first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useCounter } from './hooks/useCounter';
 
-const FirstApp = ({value}) => {
+const FirstApp = ({ value }) => {
+  // Usa el hook personalizado
+  const { counter, increment, decrement, reset } = useCounter(value);
 
-  const [counter, setCounter] = useState(value)
-
-  const handleApp = () =>{
-    setCounter(counter + 1)
-  }
-
-  const restar = () =>{
-    setCounter(counter - 1)
-  }
-
-  const reiniciar = () =>{
-    setCounter(value)
-  }
-
-  return(
+  return (
     <>
       <h1>Counter</h1>
       <span>{counter}</span>
-      <button onClick={() => handleApp()}> +1 </button>
-      <button onClick={() => restar()}> -1 </button>
-      <button onClick={() => reiniciar()}> Reinicio </button>
+      <button onClick={increment}> +1 </button>
+      <button onClick={decrement}> -1 </button>
+      <button onClick={reset}> Reinicio </button>
     </>
-  )
-  
-}
+  );
+};
 
-export default FirstApp
+export default FirstApp;
