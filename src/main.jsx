11d6371ff15ac950a356c9909e3e1c1876edabb1
JeanGargano <159099449+JeanGargano@users.noreply.github.com>
2024-10-09 18:11:07 -0500

    Primer commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import FirstApp from './App.jsx';
-import './index.css';
+import MultipleCustomHooks from "./MultipleCustomHooks";
 
-const value = 10; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FirstApp value={value} />
+    <MultipleCustomHooks />
   </StrictMode>
 );
